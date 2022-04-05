60a909b6241afbe7a953f18587c3e03b904b0f5c
ivan-icko <ivanstepanovic6@gmail.com>
2022-04-05 13:34:42 +0200

    Obrisani nepotrebni paketi iz import delova.

--- json-api-caller/src/main/java/domain/Current.java
+++ json-api-caller/src/main/java/domain/Current.java
@@ -1,6 +1,6 @@
 package domain;
 
-import java.util.Date;
+
 
 public class Current {
 
--- json-api-caller/src/main/java/main/Main1.java
+++ json-api-caller/src/main/java/main/Main1.java
@@ -1,16 +1,12 @@
 package main;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import domain.Weather;
